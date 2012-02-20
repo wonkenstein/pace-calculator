70a039bcd8a2c231a6cb2ad9b7dfca86543c7524
wonkenstein <marky.wong@gmail.com>
2012-02-20 23:55:58 +0000

    Adding pace calculation

--- example/_handler.php
+++ example/_handler.php
@@ -1,6 +1,5 @@
 <?php
 include 'Form.php';
-$ERRORS = array();
 $form = new PaceCalculatorForm();
 
 if (count($_GET)) {
@@ -15,21 +14,34 @@ if (count($_GET)) {
   $form->init($keys, $_GET);
 
 
-
   if ($valid = $form->validate()) {
     $time = $form->values['hrs'] . '.' . $form->values['mins'] . '.' . $form->values['secs'];
     $distance = ($form->values['length']) ? $form->values['length'] : $form->values['common_length'];
     $pace = $form->values['pace_hrs'] . '.' . $form->values['pace_mins'] . '.' . $form->values['pace_secs'];
     $pace_type = $form->values['pace_type'];
 
-
-
-    $form_values = array(
+    $values = array(
         'time' => $time,
-        'distance' => $distance,
+        'distance' => $distance * 1000,
         'pace' => $pace,
         'pace_type' => $pace_type,
     );
+
+    include '../PaceCalculator.php';
+    $calc = new PaceCalculator();
+
+    if ($form->values['calculator-type'] == 'pace') {
+      $pace = $calc->getPace($values['distance'], $values['time']);
+      echo $pace, '=', $distance, '::', $time;
+
+    }
+    else if ($pace_type == 'distance') {
+
+    }
+    else if ($pace_type == 'time') {
+
+    }
+
   }
 
 
